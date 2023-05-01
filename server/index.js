fed460867a3dfa7c511f3d5f442224cd40180ead
Hrutciv Yevgen <67765825+1234567890oooyt@users.noreply.github.com>
2023-05-01 16:22:51 +0300

    2

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const setup = async () => {
 
  app.use('/api', API.router);
  app.use(express.static(path.join(__dirname, '../build')));
-
+ app.use(express.static(path.join(__dirname, '../public')));
  app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
  });
