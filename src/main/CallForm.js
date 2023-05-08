737edb89c74f8e28793f89fa432310b517841465
Hrutciv Yevgen <67765825+1234567890oooyt@users.noreply.github.com>
2023-05-08 11:53:32 +0300

    10

--- src/main/CallForm.js
+++ src/main/CallForm.js
@@ -12,7 +12,7 @@ function CallForm() {
                     <ul><li className="call_form_line"><input className="form_name input_style" type="text" placeholder="Ім'я" /><input className="form_email input_style" type="text" placeholder="Ел.пошта" /></li>
                         <li className="call_form_line"><input className="form_code_number input_style" type="text" placeholder="UA +380" /><input className="form_number input_style" type="text" placeholder="Телефон" /></li>
                         <li className="call_form_line"><input className="form_date input_style" type="text" placeholder="Дата і час консультації ( за бажанням )" /></li>
-                        <li className="call_form_line"><input className="form_question input_style" type="text" placeholder="Питання" /></li>
+                        <li className="call_form_line"><textarea className="form_question input_style" type="text" placeholder="Питання" ></textarea></li>
                     </ul>
                     <button className="form_send">Надіслати</button>
                 </div>
