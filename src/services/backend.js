2707e17c49b3dd2f14b78d1d3c0c00ff8a157105
Hrutciv Yevgen <67765825+1234567890oooyt@users.noreply.github.com>
2023-05-10 17:18:41 +0300

    rrr

--- src/services/backend.js
+++ src/services/backend.js
@@ -5,34 +5,34 @@ class BackEndAPI {
    constructor() {
       this.baseUrl = process.env.REACT_APP_BASE_URL + '/api';
    }
-
+ 
    async getUniversityInfo () {
       const { data } = await axios.get(
-         `${this.baseUrl}/university-info`
+         `${this.baseUrl}/university-info?_vercel_no_cache=1`
       );
       return data;
    }
-
+ 
    async getServices () {
       const { data } = await axios.get(
-         `${this.baseUrl}/services`
+         `${this.baseUrl}/services?_vercel_no_cache=1`
       );
       return data;
    }
-
+ 
    async getArticle (articleId) {
       /**
        *  @type {{data: { title: string, blocks: [{ type: 'text', value?: string}]}}}
        */
       const { data } = await axios.get(
-         `${this.baseUrl}/articles/${articleId}`
+         `${this.baseUrl}/articles/${articleId}?_vercel_no_cache=1`
       );
       return data;
    }
-
+ 
    /**
-    * 
-    * @param {Object} data 
+    *
+    * @param {Object} data
     * @param {string} data.email
     * @param {string} data.name
     * @param {Date} data.requestedDate
@@ -40,8 +40,8 @@ class BackEndAPI {
     * @param {string} data.phoneNumber
     */
    async createRequest (data) {
-      await axios.post(`${this.baseUrl}/requests`, data);
+      await axios.post(`${this.baseUrl}/requests?_vercel_no_cache=1`, data);
    }
-}
+ }
 
 export const backEndAPI = new BackEndAPI();
